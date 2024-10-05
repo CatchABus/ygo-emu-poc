5bc0c118afec4db49b731bbc2dd1bc3ccb0f2c1a
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2024-10-06 00:48:00 +0300

    chore(database-installer): Renamed cardNum column to templateId

--- packages/database-installer/migrations/player_cards.sql
+++ packages/database-installer/migrations/player_cards.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `player_cards` (
   `id` BIGINT(20) AUTO_INCREMENT,
 	`playerId` BIGINT(20) NOT NULL,
-	`cardNum` BIGINT(20) NOT NULL,
+	`templateId` BIGINT(20) NOT NULL,
 	`count` INT(10) DEFAULT '1',
 	`isNew` TINYINT(1) DEFAULT '0',
 	PRIMARY KEY (`id`),
-	UNIQUE (`playerId`,`cardNum`)
+	UNIQUE (`playerId`,`templateId`)
 );
