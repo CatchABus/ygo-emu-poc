a6be3cbe926c183460e0a0d4794d64688e45eec6
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2024-10-06 15:25:50 +0300

    fix(database-installer): Added missing unique declaration for accounts table

--- packages/database-installer/migrations/accounts.sql
+++ packages/database-installer/migrations/accounts.sql
@@ -2,5 +2,6 @@ CREATE TABLE `accounts` (
   `id` BIGINT(20) AUTO_INCREMENT,
 	`accountName` VARCHAR(100) NOT NULL,
 	`password` VARCHAR(100) NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
+	UNIQUE (`accountName`)
 );
